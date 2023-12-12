ac7b2ace3c6b103f1e0076fc9596d7a1a3fcfdbb
user <chldbfkr456@naver.com>
2023-12-12 17:13:16 +0900

    2023-12-12 Commit - 장바구니 Cart 엔티티 생성, CartRepository 생성, 장바구니 상품 저장 테스트 구현

---
+++ ecommerce/src/main/java/springweb/ecommerce/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package springweb.ecommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springweb.ecommerce.entity.Cart;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+}
