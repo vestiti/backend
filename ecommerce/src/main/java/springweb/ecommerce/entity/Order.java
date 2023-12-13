3d96ac7ded88ac0680a9e4c2f7fc018683430ff3
user <chldbfkr456@naver.com>
2023-12-13 17:31:46 +0900

    2023-12-13 Commit - OrderItemRepository 생성, 엔티티 지연 로딩 적용

--- ecommerce/src/main/java/springweb/ecommerce/entity/Order.java
+++ ecommerce/src/main/java/springweb/ecommerce/entity/Order.java
@@ -17,7 +17,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
@@ -26,7 +26,8 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true,
+    fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
