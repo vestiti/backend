fdc0407bea794e88b750149835191343e6f5a165
user <chldbfkr456@naver.com>
2024-01-08 17:13:49 +0900

    2023-01-08 Commit - 각 엔티티에 주문 메서드 구현

--- ecommerce/src/main/java/springweb/ecommerce/entity/Item.java
+++ ecommerce/src/main/java/springweb/ecommerce/entity/Item.java
@@ -3,6 +3,7 @@ package springweb.ecommerce.entity;
 import lombok.*;
 import springweb.ecommerce.constant.ItemSellStatus;
 import springweb.ecommerce.dto.ItemFormDto;
+import springweb.ecommerce.exception.OutOfStockException;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -50,5 +51,16 @@ public class Item extends BaseEntity {
         this.stockNumber = itemFormDto.getStockNumber();
         this.itemDetail = itemFormDto.getItemDetail();
         this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
+
+    public void removeStock(int stockNumber) {
+        int restStock = this.stockNumber - stockNumber;
+
+        if(restStock < 0) {
+            throw new OutOfStockException("상품의 재고가 부족합니다. " +
+                    "(현재 재고 수량 : " + this.stockNumber + ")");
+        }
+
+        this.stockNumber = restStock;
     }
 }
