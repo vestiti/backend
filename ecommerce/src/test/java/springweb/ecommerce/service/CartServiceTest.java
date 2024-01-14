dad756883b044bb447c16491a2367c60b27b456a
user <chldbfkr456@naver.com>
2024-01-14 17:28:39 +0900

    2024-01-14 Commit - CartServiceTest 장바구니 담기 테스트 구현

---
+++ ecommerce/src/test/java/springweb/ecommerce/service/CartServiceTest.java
@@ -0,0 +1,77 @@
+package springweb.ecommerce.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import springweb.ecommerce.constant.ItemSellStatus;
+import springweb.ecommerce.dto.CartItemDto;
+import springweb.ecommerce.entity.CartItem;
+import springweb.ecommerce.entity.Item;
+import springweb.ecommerce.entity.Member;
+import springweb.ecommerce.repository.CartItemRepository;
+import springweb.ecommerce.repository.ItemRepository;
+import springweb.ecommerce.repository.MemberRepository;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+class CartServiceTest {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    CartItemRepository cartItemRepository;
+
+    public Item saveItem() {
+        Item item = Item.builder()
+                .itemName("테스트 상품")
+                .price(10000)
+                .itemDetail("테스트 상품 상세 설명")
+                .itemSellStatus(ItemSellStatus.SELL)
+                .stockNumber(100)
+                .build();
+
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember() {
+        Member member = Member.builder()
+                .email("test@email.com")
+                .build();
+
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("장바구니 담기 테스트")
+    public void addCartTest() {
+        Item item = saveItem();
+        Member member = saveMember();
+
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setCount(5);
+        cartItemDto.setItemId(item.getId());
+
+        Long cartItemId = cartService.addCart(cartItemDto, member.getEmail());
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        assertThat(cartItem.getItem().getId()).isEqualTo(item.getId());
+        assertThat(cartItemDto.getCount()).isEqualTo(cartItem.getCount());
+    }
+}
